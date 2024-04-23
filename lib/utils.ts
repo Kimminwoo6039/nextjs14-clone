f8c61c77856bcf56a0885431530c59c4adf8ad9e
kimminwoo <qkqhemforhs1@gmail.com>
2024-04-23 22:07:49 +0900

    NextJs-14 image

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,3 +7,11 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+
+
+export function getRandomInt(min:number , max:number) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
